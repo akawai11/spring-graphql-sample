bfe6a281b4648d5908b9f85d110c121874f6d843
g0z4ru <pmeiclak@gmail.com>
2020-06-30 08:41:31 +0900

    init commit

---
+++ src/main/kotlin/com/example/springgraphqlsample/SpringGraphqlSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springgraphqlsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringGraphqlSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringGraphqlSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springgraphqlsample/SpringGraphqlSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springgraphqlsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringGraphqlSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
